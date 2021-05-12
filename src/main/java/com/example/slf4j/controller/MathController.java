6e580eea8ccd98bc8cea9edc9a0dce4f4a79df11
moyu-1011 <1011847996@qq.com>
2021-05-12 13:37:05 +0800

    some update

--- src/main/java/com/example/slf4j/controller/MathController.java
+++ src/main/java/com/example/slf4j/controller/MathController.java
@@ -4,6 +4,7 @@ import com.example.slf4j.service.BaseMathService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,9 +15,10 @@ public class MathController {
     @Autowired
     private BaseMathService mathService;
 
-    @GetMapping("/test")
+    @GetMapping("/test/{num1}/{num2}")
     @ResponseBody
-    public int getAddResult(int num1, int num2) {
+    public int getAddResult(@PathVariable("num1") int num1, @PathVariable("num2") int num2) {
+        System.out.println("jenkins");
         return mathService.add(num1, num2);
     }
 
